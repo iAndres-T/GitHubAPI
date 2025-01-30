af1cb863c87e22172aa1333d0f29e941a1e7f289
PABLO ANDRES TABARES CARDONA <pablotabares316543@correo.itm.edu.co>
2025-01-30 16:42:51 -0500

    Refactorizar la función readExcel para mejorar el manejo de datos y simplificar el formateo de fechas

--- readExcel.js
+++ readExcel.js
@@ -1,7 +1,7 @@
 const xlsx = require('xlsx');
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, 'public', 'resources' ,'Inventario del GitHub.xlsx');
+const filePath = path.join(__dirname, 'public', 'resources', 'Inventario del GitHub.xlsx');
 
 function readExcel() {
   if (!fs.existsSync(filePath)) {
@@ -12,28 +12,28 @@ function readExcel() {
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
   const data = xlsx.utils.sheet_to_json(sheet, { range: 5 });
-  let json = data.slice(0, 2);
-  json = json.map(fila => { 
+  let json = data;//.slice(0, 50);
+  json = json.map(fila => {
     return {
       name: fila['NOMBRE DEL REPOSITORIO'],
       description: fila['DESCRIPCIÓN'],
       html_url: fila['URL'],
-      created_at: formatText(fila['FECHA DE CREACIÓN'], 'date'),
+      created_at: formatDate(fila['FECHA DE CREACIÓN']),
       license: fila['LICENCIA DE DEPENDENCIA'],
-      structure_file: formatText(fila['ESTRUCTURA DE CARPETAS'], 'enter'),
-      key_files: formatText(fila['ARCHIVOS CLAVE'], 'enter'),
+      structure_file: fila['ESTRUCTURA DE CARPETAS'],
+      key_files: fila['ARCHIVOS CLAVE'],
       readme: fila['ARCHIVO README.md'],
       contributing: fila['ARCHIVOS DE CONTRIBUCIÓN'],
       default_branch: fila['BRANCH PRINCIPAL'],
-      branches: formatText(fila['ESTRATEGIA DE RAMAS'], 'both'),
+      branches: fila['ESTRATEGIA DE RAMAS'],
       last_commit: fila['ULTIMO COMMIT'],
       releases: fila['ULTIMO RELEASE'],
-      dependencies: formatText(fila['DEPENDENCIAS'], 'enter'),
+      dependencies: fila['DEPENDENCIAS'],
       dependencies_problems: fila['DEPENDENCIAS CON VULNERABILIDADES'],
       security: fila['REVISIÓN DE SEGURIDAD'],
-      access: formatText(fila['PERMISOS'], 'dot'),
-      collaborators: formatText(fila['ACCESOS'], 'both'),
-      sensitive_files: formatText(fila['ARCHIVOS SENSIBLES'], 'enter'),
+      access: fila['PERMISOS'],
+      collaborators: fila['ACCESOS'],
+      sensitive_files: fila['ARCHIVOS SENSIBLES'],
       issues: fila['ISSUES ABIERTOS'],
       pull_requests: fila['PULL REQUEST ABIERTOS'],
       contributing_document: fila['DOCUMENTACIÓN DE CONTRIBUCIÓN'],
@@ -41,20 +41,21 @@ function readExcel() {
       qa: fila['PRUEBAS QA'],
       sonar: fila['SONARQUBE O HERRAMIENTAS DE CALIDAD'],
       cd: fila['DESPLIEGUE AUTOMATICO'],
+      readme2: fila['ARCHIVO README.md_1'],
       wiki: fila['WIKI O PÁGINA DE GITHUB'],
       example: fila['EJEMPLO DE USO'],
       contributing_guide: fila['GUIAS DE CONTRIBUCIÓN'],
       forks: fila['FORKs'],
       current_contributing: fila['CONTRIBUCIONES ACTIVAS'],
-      last_event: formatText(fila['ULTIMA ACTIVIDAD'], 'enter'),
+      last_event: fila['ULTIMA ACTIVIDAD'],
       update_frequency: fila['FRECUENCIA DE ACTUALIZACIONES'],
       archived_plan: fila['PLAN DE DESACTIVACIÓN O ARCHIVADO'],
       future_steps: fila['PRÓXIMOS PASOS O FEATURE'],
-      comments: formatText(fila['COMENTARIOS GENERALES'], 'dot')
+      comments: fila['COMENTARIOS GENERALES']
     }
   });
   //console.log(json.slice(0, 1));
-  return json.slice(0, 1);
+  return json;
 }
 
 function excelDateToJSDate(serial) {
@@ -63,18 +64,15 @@ function excelDateToJSDate(serial) {
   return new Date(excelEpoch.getTime() + serial * 86400000); // 86400000 ms en un día
 }
 
-function formatText(text, type) {
-  switch (type) {
-    case 'enter':
-      return text.replace(/\n/g, '<br>');
-    case 'dash':
-      return text.replace(/- /g, '');
-    case 'both':
-      return text.replace(/\n/g, '<br>').replace(/- /g, '');
-    case 'dot':
-      return text.replace(/\. /g, '<br>');
-    case 'date':
-      return excelDateToJSDate(text).toISOString().split('T')[0];
+function formatDate(date) {
+  try {
+    if (date == 'Repositorio vacío') {
+      return date;
+    }
+    return excelDateToJSDate(date).toISOString().split('T')[0];
+  } catch (error) {
+    console.error(`Error en la fecha de creación ${date}`);
+    return 'No se pudo formatear la fecha, revisar el formato en la celda de Excel.';
   }
 }
 
